1cd422295a2a2ba59a731a4aa264e2e36eb35546
Sébastien Celles <s.celles@gmail.com>
2021-05-01 17:26:08 +0200

    WIP

--- conv_vrp_jprnavmaster2cup.py
+++ conv_vrp_jprnavmaster2cup.py
@@ -1,37 +1,46 @@
+import click
 import pandas as pd
 from aerofiles.seeyou.writer import Writer
 from aerofiles.seeyou.converter import WaypointStyle
 
 
-fname = "vrp_france_jprnavmaster.csv"
+@click.command()
+@click.option(
+    "--input", default="vrp_france_jprnavmaster.csv", help="CSV filename input"
+)
+@click.option("--output", default="vrp_france.cup", help="CUP filename output")
+def main(input, output):
+    # ToDo: manually add a ; at end of first line
+    encoding = "utf8"  # "latin1"
+    df = pd.read_csv(input, encoding=encoding, sep=";")
 
-# ToDo: manually add a ; at end of first line
-encoding = "utf8"  # "latin1"
-df = pd.read_csv(fname, encoding=encoding, sep=";")
+    print(df)
 
-print(df)
-
-fname_out = "vrp_france.cup"
-with open(fname_out, "wb") as fd:
-    w = Writer(fd)
-    for (_, wpt) in df.iterrows():
-        code = wpt.CODE
-        splited_code = code.split("/")
-        if len(splited_code) == 2:
-            ad, shortname = splited_code
-            code = shortname + "@" + ad
-        else:
+    with open(output, "wb") as fd:
+        w = Writer(fd)
+        for (_, wpt) in df.iterrows():
+            code = wpt.CODE
+            # splited_code = code.split("/")
+            # if len(splited_code) == 2:
+            #    ad, shortname = splited_code
+            #    code = shortname + "@" + ad
+            # else:
+            #    shortname = wpt.CODE
             shortname = wpt.CODE
-        w.write_waypoint(
-            code,
-            shortname,
-            wpt.Pays,
-            wpt.Latitude,
-            wpt.Longitude,
-            elevation=wpt.Altitude,
-            style=WaypointStyle.NORMAL,
-            runway_direction=u"",
-            runway_length=u"",
-            frequency=u"",
-            description=wpt.Nom,
-        )
+            w.write_waypoint(
+                code,
+                shortname,
+                wpt.Pays,
+                wpt.Latitude,
+                wpt.Longitude,
+                elevation=wpt.Altitude,
+                style=WaypointStyle.NORMAL,
+                runway_direction=u"",
+                runway_length=u"",
+                frequency=u"",
+                description=wpt.Nom,
+            )
+
+
+if __name__ == "__main__":
+    main()
--- conv_vrp_jprnavmaster2littlenavmap.py
+++ conv_vrp_jprnavmaster2littlenavmap.py
@@ -1,29 +1,55 @@
+import click
 import pandas as pd
 
-fname = "vrp_france_jprnavmaster.csv"
 
-# ToDo: manually add a ; at end of first line
-encoding = "utf8"  # "latin1"
-df = pd.read_csv(fname, encoding=encoding, sep=";")
+@click.command()
+@click.option(
+    "--input", default="vrp_france_jprnavmaster.csv", help="CSV filename input"
+)
+@click.option(
+    "--output",
+    default="vrp_france_littlenavmap.csv",
+    help="LittleNavMap filename output",
+)
+def main(input, output):
+    # ToDo: manually add a ; at end of first line
+    encoding = "utf8"  # "latin1"
+    df = pd.read_csv(input, encoding=encoding, sep=";")
 
-print(df)
+    print(df)
 
-df = df.rename(columns={
-    "Cat": "Type",
-    "Pays": "Region",
-    "Nom": "Description",
-    "CODE": "Name",
-})
+    df = df.rename(
+        columns={"Cat": "Type", "Pays": "Region", "Nom": "Description", "CODE": "Name",}
+    )
 
-df["Type"] = df["Type"].map({"PtRep": "VRP"})
-df["Ident"] = ""
-df["MagneticDeclination"] = ""
-df["Tags"] = ""
+    df["Type"] = df["Type"].map({"PtRep": "VRP"})
+    df["Ident"] = ""
+    df["MagneticDeclination"] = ""
+    df["Tags"] = ""
 
-# df["Description"] = df["Description"].map(lambda s: '"' + s + '"')
-df["VisibleFrom"] = 25 # NM
-df["LastUpdateTimestamp"] = ""
+    # df["Description"] = df["Description"].map(lambda s: '"' + s + '"')
+    df["VisibleFrom"] = 25  # NM
+    df["LastUpdateTimestamp"] = ""
 
-df = df[["Type", "Name", "Ident", "Latitude", "Longitude", "Altitude", "MagneticDeclination", "Tags", "Description", "Region", "VisibleFrom", "LastUpdateTimestamp"]]
-print(df)
-df.to_csv("vrp_france_littlenavmap.csv", sep=",", index=False, header=False)
+    df = df[
+        [
+            "Type",
+            "Name",
+            "Ident",
+            "Latitude",
+            "Longitude",
+            "Altitude",
+            "MagneticDeclination",
+            "Tags",
+            "Description",
+            "Region",
+            "VisibleFrom",
+            "LastUpdateTimestamp",
+        ]
+    ]
+    print(df)
+    df.to_csv(output, sep=",", index=False, header=False)
+
+
+if __name__ == "__main__":
+    main()
